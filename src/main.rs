128c8b029ee122f11c0916229c3a7512987559af
Peter Olds <polds@kyanicorp.com>
2016-07-15 14:42:40 -0600

    Let’s try this

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+extern crate winapi;
+
+use winapi::user32;
+
 fn main() {
     println!("Hello, world!");
+
+    user32::MessageBoxExW(None, Some("lpText"), Some("lpCaption"), Some("uType"), Some("wLanguageId"))
 }
