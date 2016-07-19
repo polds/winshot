a1da2f9cfc1c413411f75ab3f30cdb00797f3818
Peter Olds <polds@kyanicorp.com>
2016-07-19 07:55:16 -0600

    casting?

--- src/main.rs
+++ src/main.rs
@@ -157,8 +157,8 @@ fn main() {
 		let style = winapi::winuser::WS_POPUP;
 
 		let win = user32::CreateWindowExW(exstyle,
-			m_class_name,
-			m_class_name,
+			m_class_name.as_ptr() as *mut _,
+			m_class_name.as_ptr() as *mut _,
 			style,
 			// dimensions
 			0, 0, rect.x2, rect.y2,
